d527e77d1b67e429f2e324e20cecc8f98f7f9bfb
Mazen Mirza <84817881+MMazenA@users.noreply.github.com>
2023-02-10 23:17:43 -0500

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -77,7 +77,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   {showResume && (
                     <Button
                       onClick={() =>
-                        window.open("mailto:hello@chetanverma.com")
+                        window.open("mailto:hh8001@wayne.edu")
                       }
                     >
                       Resume
